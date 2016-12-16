ef1a2f4e4912f17a3a7375973a658a2be66db152
KYONET <manuboncha@gmail.com>
2016-12-16 16:58:33 -0500

    Primer Commit - Entrega

---
+++ app/src/main/java/com/manuboncha/aplicacion_bienvenida/FullscreenActivity.java
@@ -0,0 +1,29 @@
+package com.manuboncha.aplicacion_bienvenida;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_principal);
+
+
+
+
+
+    }
+
+}
